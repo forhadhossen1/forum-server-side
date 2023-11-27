659e01145d1f0227bd251a4b2f66d91836e97c7d
forhadhossen1 <mr.forhad1030@gamil.com>
2023-11-27 16:11:14 +0600

    get forum data from mongo db

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const forumCollection = client.db("forumDB").collection('forum');
+
+
+
+    app.get('/forum', async (req, res) => {
+      const result = await forumCollection.find().toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
